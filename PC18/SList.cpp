acd28d708abbd00c9c80c78727c675b3aaf5ba8e
OpenShift guest <stacyrios@yahoo.com>
2014-04-09 10:45:53 -0400

    PC18

---
+++ PC18/SList.cpp
@@ -0,0 +1,26 @@
+SLNode::SLNode()
+{
+    
+}
+SLNode::~SLNode()
+{
+    
+}
+void SLNode::insertHead(int size)
+{
+    
+}
+void SLNode::removeHead()
+{
+    
+}
+unsigned int SLNode::getSize() const
+{
+    return size;
+}
+string SLNode::toString() const
+{
+    return string;
+}
+	
+	
